a8b3a489c6be22830bf084a427ec240c85cd2480
Petr Mruzek <petr.mruzek@grit.cz>
2024-06-10 10:41:41 +0200

    OBACKLOG-15475 ZK 10 - přestylování tématu pro novou verzi

--- preview/zk/example/ThemePreviewApp.java
+++ preview/zk/example/ThemePreviewApp.java
@@ -7,8 +7,8 @@ import org.zkoss.lang.Library;
 @SpringBootApplication
 public class ThemePreviewApp {
     public static void main(String[] args) {
-        System.setProperty("zk.homepage", "preview");
-        Library.setProperty("org.zkoss.theme.preferred", "___THEME_NAME___");
+        System.setProperty("zk.homepage", "main");
+        Library.setProperty("org.zkoss.theme.preferred", "zk10theme");
         //disable caches for development
         Library.setProperty("org.zkoss.zk.ZUML.cache", "false");
         Library.setProperty("org.zkoss.zk.WPD.cache", "false");
